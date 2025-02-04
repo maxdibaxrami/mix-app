f269a2050f4df78724e9a3a7ea5b9e8ceb17e38a
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-02-04 12:44:31 +0300

    m

--- frontend/src/api/base.ts
+++ frontend/src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://copychic.ru',  // Replace with your API base URL
+  baseURL: 'https://copychic.ru/api',  // Replace with your API base URL
   timeout: 40000 ,                      // Set a timeout for requests
 });
 
